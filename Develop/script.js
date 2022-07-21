9efe139b0de05395c220c3820185a8687c5d53c0
Clay McKee <cmckee120993@gmail.com>
2022-07-21 12:50:50 -0700

    Completed a while loop to create the password and verified that all brackets had an opening and closing. Ran into an issue with getRandomCharacter, so ready to verify the functionality of logging a new password to the console before outputting it to the user on the webpage.

--- Develop/script.js
+++ Develop/script.js
@@ -9,18 +9,21 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ";", ":", ">", "<", "=", "?","@", "[", "]", "{", "}", "^", "_", "`", "~", "|"];
 
-console.log()
 
 
 // Write password to the #password input
 function writePassword() {
   var wantsUpper = confirm("Would you like uppercased letters in your password? 'OK' for yes, 'Cancel' for no.");
+  console.log(wantsUpper);
   var wantsLower = confirm("Would you like lowercased letters in your password? 'OK' for yes, 'Cancel' for no.");
+  console.log(wantsLower);
   var wantsNumbers = confirm("Would you like numbers in your password? 'OK' for yes, 'Cancel' for no.");
+  console.log(wantsNumbers);
   var wantsSpecial = confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel' for no.");
-  var numberOfCharacters = prompt("How many characters would you like in your password? Write a number between 8 and 128.");
- 
-
+  console.log(wantsSpecial);
+  var numberOfCharacters = parseInt(prompt("How many characters would you like in your password? Write a number between 8 and 128."));
+  console.log(numberOfCharacters);
+  var newPassword = [];
   if (wantsUpper === false && wantsLower === false && wantsNumbers === false && wantsSpecial === false) {
     window.alert("You must choose at least one character type for your password. Please try again");
   }
@@ -29,10 +32,32 @@ function writePassword() {
     window.alert("Your password must be at least 8 characters long and can be no longer than 128 characters. Please try again.")
   }
 
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //passwordText.value = password;
+  
+  while (newPassword < numberOfCharacters) {
+    if(wantsUpper && newPassword.length < numberOfCharacters) {
+      var randomUpper = getRandomCharacter(upper);
+      newPassword.push(randomUpper);
+    }
+    if(wantsLower && newPassword.length < numberOfCharacters) {
+      var randomLower = getRandomCharacter(lower);
+      newPassword.push(randomLower);
+    }
+    if(wantsNumbers && newPassword.length < numberOfCharacters) {
+      var randomNumber = getRandomCharacter(numbers);
+      newPassword.push(randomNumber);
+    }
+    if(wantsSpecial && newPassword.length < numberOfCharacters) {
+      var randomSpecial = getRandomCharacter(special);
+      newPassword.push(randomSpecial);
+    }
+  }
+console.log(newPassword)
 }
+
+//var password = generatePassword();
+//var passwordText = document.querySelector("#password");
+//passwordText.value = password;
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
