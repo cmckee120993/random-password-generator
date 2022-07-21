3d1cd0981c69b8206d6e06b035abee413251703d
Clay McKee <cmckee120993@gmail.com>
2022-07-21 13:32:44 -0700

    I have gotten the program to output numbers of the items in the array to the console, but I cannot get it to output actual characters from the list. I'm attempting a new random generator by using a function rather than a variable in hopes that it may solve the problem.

--- Develop/script.js
+++ Develop/script.js
@@ -3,27 +3,37 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ";", ":", ">", "<", "=", "?","@", "[", "]", "{", "}", "^", "_", "`", "~", "|"];
+var newPassword = [];
 
-
+function randomCharacter() { 
+  var upperCase = Math.floor(Math.random() * upper.length);
+  var item = upper[upperCase];
+  return item;
+}
+console.log(randomCharacter());
 
 // Write password to the #password input
 function writePassword() {
+
   var wantsUpper = confirm("Would you like uppercased letters in your password? 'OK' for yes, 'Cancel' for no.");
   console.log(wantsUpper);
+ 
   var wantsLower = confirm("Would you like lowercased letters in your password? 'OK' for yes, 'Cancel' for no.");
   console.log(wantsLower);
+ 
   var wantsNumbers = confirm("Would you like numbers in your password? 'OK' for yes, 'Cancel' for no.");
   console.log(wantsNumbers);
+ 
   var wantsSpecial = confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel' for no.");
   console.log(wantsSpecial);
+ 
   var numberOfCharacters = parseInt(prompt("How many characters would you like in your password? Write a number between 8 and 128."));
   console.log(numberOfCharacters);
-  var newPassword = [];
+
   if (wantsUpper === false && wantsLower === false && wantsNumbers === false && wantsSpecial === false) {
     window.alert("You must choose at least one character type for your password. Please try again");
   }
@@ -33,26 +43,36 @@ function writePassword() {
   }
 
   
-  while (newPassword < numberOfCharacters) {
+  while (newPassword.length < numberOfCharacters) {
+
     if(wantsUpper && newPassword.length < numberOfCharacters) {
-      var randomUpper = getRandomCharacter(upper);
+      function randomUpper () {
+      var uppercase = Math.floor(Math.random()*upper.length);
+      var upperCharacter = uppser[uppercase];
+      return upperCharacter;
       newPassword.push(randomUpper);
     }
+  }
     if(wantsLower && newPassword.length < numberOfCharacters) {
-      var randomLower = getRandomCharacter(lower);
+      var randomLower = Math.floor(Math.random()*lower.length);
+      console.log(randomLower);
       newPassword.push(randomLower);
     }
     if(wantsNumbers && newPassword.length < numberOfCharacters) {
-      var randomNumber = getRandomCharacter(numbers);
+      var randomNumber = Math.floor(Math.random() *numbers.length);
+      console.log(randomNumber);
       newPassword.push(randomNumber);
     }
     if(wantsSpecial && newPassword.length < numberOfCharacters) {
-      var randomSpecial = getRandomCharacter(special);
+      var randomSpecial = Math.floor(Math.random()*special.length);
+      console.log(randomSpecial);
       newPassword.push(randomSpecial);
     }
   }
-console.log(newPassword)
 }
+console.log(newPassword);
+
+writePassword();
 
 //var password = generatePassword();
 //var passwordText = document.querySelector("#password");
