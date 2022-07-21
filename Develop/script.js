1c8252f224a26d556f1a53ca68a2648ee70175f8
Clay McKee <cmckee120993@gmail.com>
2022-07-21 09:23:20 -0700

    First, I set variables to arrays for each of the characters that could be included into the password. Next, I changed the prompts to confirms so that the information would be converted into a boolean rather than a string (which I would then have to convert to a boolean). Finally, I added a prompt that asks for user input about the number of characters they desire in their password.

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,18 @@
 
 
 // Get references to the #generate element
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ";", ":", ">", "<", "=", "?","@", "[", "]", "{", "}", "^", "_", "`", "~", "|"];
 var generateBtn = document.querySelector("#generate");
 
 function generate () {
-  prompt ("Would you like numbers in your password? Type Y for yes or N for no.");
-  prompt ("Would you like special characters in your password? Type Y for yes or N for no.");
-  prompt ("Would you like upper case letters in your password? Type Y for yes or N for no.");
-  prompt ("Would you like lowercase letters in your password? Type Y for yes and N for no.")
+  confirm ("Would you like numbers in your password? Click 'OK' for yes and 'Cancel' for no.");
+  confirm ("Would you like special characters in your password? Click 'OK' for yes and 'Cancel' for no.");
+  confirm ("Would you like upper case letters in your password? Click 'OK' for yes and 'Cancel' for no.");
+  confirm ("Would you like lowercase letters in your password? Click 'OK' for yes and 'Cancel' for no.");
+  prompt ("How many characters would you like in your password? Enter a number between 8 and 128.")
 }
 
 // Write password to the #password input
